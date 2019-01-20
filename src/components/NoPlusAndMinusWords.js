7dec479cd950caee7af0ddca458325707362120b
thesurok <surkiss2@gmail.com>
2019-01-20 19:45:29 +0200

    add default prepositions and favicon link

--- src/components/NoPlusAndMinusWords.js
+++ src/components/NoPlusAndMinusWords.js
@@ -1,42 +1,48 @@
-import React from 'react';
+import React from "react";
 
-const NoPlusAndMinusWords = ({onNoPlusChange, onMinuswordsChange, onLevelChange}) => (
-    <div>
-        <label>
-            No plus before
-            <input type="text"
-                onChange={onNoPlusChange}
-            />
-        </label>
-        <label>
-            Minuswords
-            <textarea
-                onChange={onMinuswordsChange}
-            />
-        </label>
-        <div className="row">
-            <label className="radio-container">
-                Campaign level
-                <input type="radio"
-                    name="level"
-                    value="campaign-level"
-                    onClick={onLevelChange}
-                />
-                <span className="custom-radio"><span className="custom-radio-inner_circle"></span></span>
-            </label>
-        </div>
-        <div className="row">
-            <label className="radio-container">
-                Group level
-                <input type="radio"
-                    name="level"
-                    value="group-level"
-                    onClick={onLevelChange}
-                />
-                <span className="custom-radio"><span className="custom-radio-inner_circle"></span></span>
-            </label>
-        </div>
+const NoPlusAndMinusWords = ({
+  onNoPlusChange,
+  onMinuswordsChange,
+  onLevelChange
+}) => (
+  <div>
+    <label>
+      No plus before
+      <input id="no-plus-before" type="text" onChange={onNoPlusChange} />
+    </label>
+    <label>
+      Minuswords
+      <textarea onChange={onMinuswordsChange} />
+    </label>
+    <div className="row">
+      <label className="radio-container">
+        Campaign level
+        <input
+          type="radio"
+          name="level"
+          value="campaign-level"
+          onClick={onLevelChange}
+        />
+        <span className="custom-radio">
+          <span className="custom-radio-inner_circle" />
+        </span>
+      </label>
     </div>
+    <div className="row">
+      <label className="radio-container">
+        Group level
+        <input
+          type="radio"
+          name="level"
+          value="group-level"
+          onClick={onLevelChange}
+        />
+        <span className="custom-radio">
+          <span className="custom-radio-inner_circle" />
+        </span>
+      </label>
+    </div>
+  </div>
 );
 
 export default NoPlusAndMinusWords;
