a9bc70ddad83798842797e59838550383e968fd7
unknown <surkiss2@gmail.com>
2019-08-05 10:21:43 +0300

    modify minuswords to become phrase criterion type when wrapped in quotes

--- src/components/MinusWordsRow.js
+++ src/components/MinusWordsRow.js
@@ -3,9 +3,12 @@ import React from 'react';
 const MinusWordsRow = (props) => (
     <tr>
         <td name="Campaign Name">{props.campaignName}</td>
-        <td name="Group Name">{props.minuswordsCritType === 'group-level' ? props.groupName:null}</td>
+        <td name="Group Name">{props.minuswordsCritType === 'group-level' ? props.groupName : null}</td>
         <td name="Keyword">{props.minusword}</td>
-        <td name="Criterion Type">{props.minuswordsCritType === 'group-level' ? 'Negative Broad' : 'Campaign Negative Broad'}</td>
+        <td name="Criterion Type">{
+            props.minusword[0] === '"' ? (props.minuswordsCritType === 'group-level' ? 'Negative Phrase' : 'Campaign Negative Phrase') :
+                (props.minuswordsCritType === 'group-level' ? 'Negative Broad' : 'Campaign Negative Broad')
+        }</td>
         <td name="Headline 1"></td>
         <td name="Headline 2"></td>
         <td name="Description"></td>
